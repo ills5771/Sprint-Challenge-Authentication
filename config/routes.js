c8f9ff4356a60aa6c30e02ea1cd06ee376ec1404
ills5771 <ilyayelizarov1@gmail.com>
2019-04-05 09:06:06 -0700

    implements login function

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 const bcrypt = require("bcryptjs");
 const { authenticate } = require("../auth/authenticate");
 const Users = require("./users-model.js");
+const jwt = require("jsonwebtoken");
+const secret = require("../api/secrets").jwtSecret;
 
 module.exports = server => {
   server.post("/api/register", register);
@@ -26,7 +28,32 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Users.getBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secret, options);
 }
 
 function getJokes(req, res) {
